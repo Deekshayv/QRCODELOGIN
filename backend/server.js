46dedfc20ae25725c89287b064d999409bae07e0
25sripriya <98088566+25sripriya@users.noreply.github.com>
2025-03-13 11:22:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -102,7 +102,7 @@ app.post("/scan-qr", (req, res) => {
 });
 
 // Start the server
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log('Server running on port ${PORT}');
 });
